bca11805a7d30684a3da345a146a28b2d2bb1dcb
Nelsen-Nariman <nelsennarimann@gmail.com>
2022-01-13 20:18:18 +0700

    Initial Commit

---
+++ src/Main/Main.java
@@ -0,0 +1,15 @@
+package Main;
+
+public class Main {
+
+	public Main() {
+		// TODO Auto-generated constructor stub
+	}
+
+	public static void main(String[] args) {
+		System.out.println("Hello world!");
+		System.out.println("Hello world 2!");
+
+	}
+
+}
