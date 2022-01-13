51937a99b33d73df42f0935490fb6fce230a304d
Nelsen-Nariman <nelsennarimann@gmail.com>
2022-01-13 20:49:59 +0700

    Hello world dihapus

--- src/Main/Main.java
+++ src/Main/Main.java
@@ -15,7 +15,6 @@ public class Main {
 //>>>>>>> fd77704f8586c0263aea30a1243e664b99b21121
 //=======
 //=======
-		System.out.println("Hello world!");
 //>>>>>>> fd77704f8586c0263aea30a1243e664b99b21121
 //>>>>>>> parent of e58dc77 (NewNew 2)
 
