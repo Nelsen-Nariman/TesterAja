e58dc77a968943c197ef200250b033bcaa40005a
Nelsen-Nariman <nelsennarimann@gmail.com>
2022-01-13 20:43:23 +0700

    NewNew 2

--- src/Main/Main.java
+++ src/Main/Main.java
@@ -7,11 +7,12 @@ public class Main {
 	}
 
 	public static void main(String[] args) {
-<<<<<<< HEAD
+//<<<<<<< HEAD
 		
-=======
+//=======
 		System.out.println("Hello world!");
->>>>>>> fd77704f8586c0263aea30a1243e664b99b21121
+		System.out.println("Hello world! 2");
+//>>>>>>> fd77704f8586c0263aea30a1243e664b99b21121
 
 	}
 
