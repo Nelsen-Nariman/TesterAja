72dffd9fa641d64e82e8780653e5e787db791735
Nelsen-Nariman <nelsennarimann@gmail.com>
2022-01-13 20:46:49 +0700

    After Revert

--- src/Main/Main.java
+++ src/Main/Main.java
@@ -9,9 +9,15 @@ public class Main {
 	public static void main(String[] args) {
 //<<<<<<< HEAD
 		
+//<<<<<<< HEAD
 //=======
 		
 //>>>>>>> fd77704f8586c0263aea30a1243e664b99b21121
+//=======
+//=======
+		System.out.println("Hello world!");
+//>>>>>>> fd77704f8586c0263aea30a1243e664b99b21121
+//>>>>>>> parent of e58dc77 (NewNew 2)
 
 	}
 
